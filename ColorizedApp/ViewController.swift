2dbf5bf980a6db9e7775684f57bf3091c11f5570
Artem Yurchenko <yourchencore@yandex.ru>
2021-08-23 14:04:19 +0200

    create colorise mixing app interface want to rebuild constraints

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var firstSliderValue: UILabel!
+    @IBOutlet var secondSliderValue: UILabel!
+    @IBOutlet var thirdSliderValue: UILabel!
+    
+    
+    
+    @IBOutlet var firstSlider: UISlider!
+    @IBOutlet var secondSlider: UISlider!
+    @IBOutlet var thirdSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstSliderValue.text = String(firstSlider.value)
+        mainView.layer.cornerRadius = 10
+        
     }
-
-
+    
+    func changeColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(firstSlider.value), green: CGFloat(secondSlider.value), blue: CGFloat(thirdSlider.value), alpha: 1)
+    }
+    
+    @IBAction func firstSliderAction() {
+        firstSliderValue.text = String(firstSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func secondSliderAction() {
+        secondSliderValue.text = String(secondSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func thirdSliderAction() {
+        thirdSliderValue.text = String(thirdSlider.value)
+        changeColor()
+    }
 }
 
